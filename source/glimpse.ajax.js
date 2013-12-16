4009441d20631e54444b37964068507b44ece00e
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-12-16 16:06:58 -0500

    Fix #32 - Now only modifies headers for same origin

--- source/glimpse.ajax.js
+++ source/glimpse.ajax.js
@@ -185,15 +185,33 @@
             context.hasTried = null;
         };
     
-    var send = XMLHttpRequest.prototype.send;
-    XMLHttpRequest.prototype.send = function() { 
-        this.setRequestHeader("Glimpse-Parent-RequestID", data.baseData().requestId);
+    //var send = XMLHttpRequest.prototype.send;
+    //XMLHttpRequest.prototype.send = function() {
+    //    console.log(arguments);
+    //    try {
+    //        this.setRequestHeader("Glimpse-Parent-RequestID", data.baseData().requestId);
+    //    }catch(e) {  
+    //        console.log('Error occurred');
+    //        console.log(e);
+    //    }
         
-        pubsub.publish('trigger.ajax.request.send');
+    //    pubsub.publish('trigger.ajax.request.send');
         
-        send.apply(this, arguments);
-    };
+    //    send.apply(this, arguments);
+    //};
     
+    var open = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function(method, uri) { 
+        open.apply(this, arguments);
+          
+        if (uri && (!(uri.indexOf('http://') == 0 || uri.indexOf('https://') == 0 || uri.indexOf('//') == 0) || (uri.substring(uri.indexOf('//') + 2, uri.length) + '/').indexOf(window.location.host + '/') == 0)) {
+            this.setRequestHeader("Glimpse-Parent-RequestID", data.baseData().requestId);
+
+            pubsub.publish('trigger.ajax.request.send');
+        }
+    };
+
+
     pubsub.subscribe('trigger.shell.subscriptions', wireListeners);
     pubsub.subscribe('action.panel.hiding.ajax', deactivate); 
     pubsub.subscribe('action.panel.showing.ajax', activate); 
