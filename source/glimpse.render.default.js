cbc66ed47ed7ce93b525b9334ed165870f16fe9a
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-12-27 15:30:11 -0500

    Fix #37 - Added required start and stop focus events

--- source/glimpse.render.default.js
+++ source/glimpse.render.default.js
@@ -20,9 +20,30 @@
         selected = function (args) {
             if (!args.forced)
                 settings.local('view', args.key);
+        },
+        currentTab = '', 
+        focusStart = function(key, isOpening) {
+            if (key)
+                currentTab = key;
+            
+            if (currentTab) { 
+                pubsub.publish('action.tab.focus.start.' + currentTab, { isOpening: isOpening, key: currentTab });
+            }
+        },
+        focusStop = function(userTermination, switchingTab, pageRedirect) { 
+            if (currentTab) { 
+                pubsub.publish('action.tab.focus.stop.' + currentTab, { userTermination: userTermination, switchingTab: switchingTab, pageRedirect: pageRedirect, key: currentTab }); 
+                currentTab = '';
+            }
         };
 
     pubsub.subscribe('trigger.shell.ready', readyOpen);
     pubsub.subscribe('action.tab.inserted', readySelect);
     pubsub.subscribe('trigger.tab.select', selected);
+    pubsub.subscribe('action.panel.showing', function(args) { focusStart(args.key, false); });
+    pubsub.subscribe('action.shell.opening', function() { focusStart(null, true); });
+    pubsub.subscribe('action.shell.closeing', function() { focusStop(true, false, false); }); 
+    pubsub.subscribe('action.shell.minimizing', function() { focusStop(true, false, false); }); 
+    pubsub.subscribe('action.panel.hiding', function() { focusStop(false, true, false); });
+    window.onbeforeunload = function() { focusStop(false, false, true); };
 })(glimpse.settings, glimpse.pubsub, glimpse.elements);
