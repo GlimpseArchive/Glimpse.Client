a61d0fb3efdbb93536f1cb7bc08949c6e4c40b12
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-07-24 10:11:34 +0100

    Add ID to session and break out title

--- src/request/components/request-session-item-view.jsx
+++ src/request/components/request-session-item-view.jsx
@@ -4,13 +4,13 @@ module.exports = React.createClass({
     render: function() {
         var session = this.props.session;
         return (
-            <div className="request-session-item-holder">
+            <div className="request-session-item-holder" onClick={this._onClick}>
                 <table className="table table-bordered">
                     <tr>
                         <td rowSpan="2">
                             <img src={session.url} width="40" />
                         </td>
-                        <td>{session.id} ({session.count})</td>
+                        <td>{session.title} ({session.count})</td>
                     </tr>
                     <tr>
                         <td>{session.last}</td>
@@ -18,5 +18,8 @@ module.exports = React.createClass({
                 </table>
             </div>
         );
+    },
+    _onClick: function() {
+        console.log(' - ', this.props.session.id);
     }
 });
--- test/fake/fake-request-session-data.js
+++ test/fake/fake-request-session-data.js
@@ -1,20 +1,20 @@
 var chance = require('./fake-extension.js'),
     glimpse = require('glimpse'),
-    peopleMaster = [ { name: 'Anthony', url: 'https://avatars.githubusercontent.com/u/585619' },
-        { name: 'Nik', url: 'https://avatars.githubusercontent.com/u/199026' },
-        { name: 'Christophe', url: 'https://avatars.githubusercontent.com/u/1467346' },
-        { name: 'Bjorn', url: 'https://avatars.githubusercontent.com/u/1607579' },
-        { name: 'Ian', url: 'https://avatars.githubusercontent.com/u/52329?' },
-        { name: 'Keith', url: 'https://avatars.githubusercontent.com/u/133987?' },
-        { name: 'Aaron', url: 'https://avatars.githubusercontent.com/u/434140?' },
-        { name: 'Jeff', url: 'https://avatars.githubusercontent.com/u/683658?' },
-        { name: 'Kristian', url: 'https://avatars.githubusercontent.com/u/582487?' },
-        { name: 'James', url: 'https://avatars.githubusercontent.com/u/1197383?' } ],
+    peopleMaster = [ { id: chance.guid(), name: 'Anthony', url: 'https://avatars.githubusercontent.com/u/585619' },
+        { id: chance.guid(), name: 'Nik', url: 'https://avatars.githubusercontent.com/u/199026' },
+        { id: chance.guid(), name: 'Christophe', url: 'https://avatars.githubusercontent.com/u/1467346' },
+        { id: chance.guid(), name: 'Bjorn', url: 'https://avatars.githubusercontent.com/u/1607579' },
+        { id: chance.guid(), name: 'Ian', url: 'https://avatars.githubusercontent.com/u/52329?' },
+        { id: chance.guid(), name: 'Keith', url: 'https://avatars.githubusercontent.com/u/133987?' },
+        { id: chance.guid(), name: 'Aaron', url: 'https://avatars.githubusercontent.com/u/434140?' },
+        { id: chance.guid(), name: 'Jeff', url: 'https://avatars.githubusercontent.com/u/683658?' },
+        { id: chance.guid(), name: 'Kristian', url: 'https://avatars.githubusercontent.com/u/582487?' },
+        { id: chance.guid(), name: 'James', url: 'https://avatars.githubusercontent.com/u/1197383?' } ],
     people = chance.pick(peopleMaster, chance.integerRange(3, peopleMaster.length));
 
 function publishSession() {
     var person = chance.pick(people),
-        item = { id: person.name, url: person.url, count: chance.integerRange(1, 30), last: (chance.integerRange(1, 45) + ' sec ago ') };
+        item = { id: person.id, title: person.name, url: person.url, count: chance.integerRange(1, 30), last: (chance.integerRange(1, 45) + ' sec ago') };
 
     glimpse.emit('data.request.session.update', item);
 
