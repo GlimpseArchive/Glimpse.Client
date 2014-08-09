4c6b45d140e8c13bfbaf97918319dd8c8af929eb
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-08-08 21:24:55 -0400

    Add missing container class/element

--- src/request/components/request-view.jsx
+++ src/request/components/request-view.jsx
@@ -6,15 +6,17 @@ var React = require('react'),
 module.exports = React.createClass({
   render: function() {
     return (
-        <div className="row">
-            <div className="col-md-2 request-user-holder-outer">
-                <User />
-            </div>
-            <div className="col-md-7 request-entry-holder-outer">
-                <Entry />
-            </div>
-            <div className="col-md-3 request-filter-holder-outer">
-                <Filter />
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-2 request-user-holder-outer">
+                    <User />
+                </div>
+                <div className="col-md-7 request-entry-holder-outer">
+                    <Entry />
+                </div>
+                <div className="col-md-3 request-filter-holder-outer">
+                    <Filter />
+                </div>
             </div>
         </div>
     );
