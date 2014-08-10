33f7cbd804244528bce4286f7f8806feb6f8d159
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-08-10 17:00:12 -0400

    Implement a basic version of filtering based on use selection

--- src/request/stores/request-entry-store.js
+++ src/request/stores/request-entry-store.js
@@ -1,12 +1,73 @@
 var glimpse = require('glimpse'),
-    _requests = [];
+    _requests = [],
+    _filteredRequests = [],
+    _filters = {};
+
+function requestsChanged(targetRequests) {
+    glimpse.emit('shell.request.summary.changed', targetRequests);
+}
+
+// TODO: This needs to be refactored to dynamically lookup records, etc
+// TODO: Even at the moment this is going to need to be refactored
+var filterRequests = (function() {
+        function hasFilters(filters) {
+            return filters.userId;
+        }
+
+        function checkMatch(request, filters) {
+            return (request.user.id === filters.userId);
+        }
+
+        function applyFilters(sourceRequests, destinationRequests, filters) {
+            var matchFound = false;
+
+            for (var i = sourceRequests.length - 1; i >= 0; i--) {
+                var sourceRequest = sourceRequests[i];
+
+                if (checkMatch(sourceRequest, filters)) {
+                    destinationRequests.unshift(sourceRequest);
+                    matchFound = true;
+                }
+            }
+
+            return matchFound;
+        }
+
+        return function(allRequests, newRequests, filterHasChanged) {
+            if (hasFilters(_filters)) {
+                var sourceRequests = newRequests;
+
+                if (filterHasChanged) {
+                    sourceRequests = allRequests;
+                    _filteredRequests = [];
+                }
+
+                if (applyFilters(sourceRequests, _filteredRequests, _filters)) {
+                    requestsChanged(_filteredRequests);
+                }
+            }
+            else {
+                requestsChanged(_requests);
+            }
+        };
+    })();
+
+(function() {
+    function userSwitch(payload) {
+        _filters.userId = payload.userId;
+
+        filterRequests(_requests, null, true);
+    }
+
+    glimpse.on('shell.request.user.selected', userSwitch);
+})();
 
 (function() {
     function dataFound(payload) {
         // TODO: Really bad hack to get things going atm
         _requests = payload.allRequests;
 
-        glimpse.emit('shell.request.summary.changed', _requests);
+        filterRequests(_requests, payload.newRequests, false);
     }
 
     // External data coming in
