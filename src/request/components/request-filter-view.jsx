7c2fe256a239dbb4375288601219be04de8dfeb8
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-08-12 11:18:20 -0400

    Add form elements and filter event to request filter view

--- src/request/components/request-filter-view.jsx
+++ src/request/components/request-filter-view.jsx
@@ -1,12 +1,43 @@
-var React = require('react');
+var glimpse = require('glimpse'),
+    React = require('react'),
+    LinkedStateMixin = React.addons.LinkedStateMixin;
 
 module.exports = React.createClass({
+    mixins: [ LinkedStateMixin ],
+    getInitialState: function() {
+        return {
+                uri: '',
+                method: '',
+                contentType: '',
+                statusCode: ''
+            };
+    },
     render: function() {
         return (
             <div className="request-session-holder">
                 <h2>Filter</h2>
-                <em>No found filters.</em>
+
+                <div>
+                    <label htmlFor="request-fitler-uri">Uri</label><br />
+                    <input type="text" id="request-fitler-uri" valueLink={this.linkState('uri')} />
+                </div><br />
+                <div>
+                    <label htmlFor="request-fitler-method">Method</label><br />
+                    <input type="text" id="request-fitler-method" valueLink={this.linkState('method')} />
+                </div><br />
+                <div>
+                    <label htmlFor="request-fitler-contentType">Content Type</label><br />
+                    <input type="text" id="request-fitler-contentType" valueLink={this.linkState('contentType')} />
+                </div><br />
+                <div>
+                    <label htmlFor="request-fitler-statusCode">Status Code</label><br />
+                    <input type="text" id="request-fitler-statusCode" valueLink={this.linkState('statusCode')} />
+                </div><br />
+                <input type="button" value="Filter" onClick={this._onFilter} />
             </div>
         );
+    },
+    _onFilter: function() {
+        glimpse.emit('shell.request.filter.updated', this.state);    
     }
 });
