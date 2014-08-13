110051805a86a4e3807765c7cf05840f70452d32
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-08-13 12:26:41 -0400

    Switch over index.js to use compile switches

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@
 var requestRepository = require('./request/repository/request-repository.js'),
     sessionRepository = require('./request/repository/user-repository.js');
 
-// TODO: Don't want to always include these
-require('diagnostics');
-require('fake');
+if (DIAGNOSTICS) {
+    require('diagnostics');
+}
+if (FAKE_SERVER) {
+    require('fake');
+}
 
 var shell = require('shell/shell.js');
 
