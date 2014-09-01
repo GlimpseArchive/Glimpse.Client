65ed02eb1436949a732675b055af9d301615ffb9
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-08-31 22:29:56 -0500

    Shift logic of whether details should be shown from request-view to request-detail-view

--- src/request/components/request-detail-view.jsx
+++ src/request/components/request-detail-view.jsx
@@ -1,39 +1,41 @@
 require('../stores/request-detail-store.js');
 
 var glimpse = require('glimpse'),
-    React = require('react');
+    React = require('react'),
+    cx = React.addons.classSet,
+    EmitterMixin = require('../../lib/components/emitter-mixin.jsx');
 
-/*
-function getState(allSummaries) {
+function getState(state) {
     return {
-        allSummaries: allSummaries || []
-    };
+            request: state ? state.request : null
+        };
 }
-*/
 
 module.exports = React.createClass({
-    /*
+    mixins: [ EmitterMixin ],
+    componentDidMount: function() {
+        this.addListener('shell.request.detail.changed', this._requestDetailChanged);
+    },
     getInitialState: function() {
         return getState();
     },
-    // TODO: Get rid of this boiler plate code via a mixin
-    componentDidMount: function() {
-        this._entryChangedOn = glimpse.on('shell.request.detail.changed', this._entryChanged);
-    },
-    componentWillUnmount: function() {
-        glimpse.off(this._entryChangedOn);
-    },
-    */
     render: function() {
-        return (
-            <div className="request-detail-holder">
-                <h2>Detail</h2>
-            </div>
-        );
+        var request = this.state.request;
+
+        if (request) {
+            return (
+                <div className="col-md-10 col-md-offset-2 request-detail-holder-outer">
+                    <div className="request-detail-holder">
+                        <h2>Detail - {request.id}</h2>
+                    </div>
+                </div>
+            );
+        }
+
+        // TODO: Need to work on doing this better
+        return <div></div>;
+    },
+    _requestDetailChanged: function(state) {
+        this.setState(getState({ request: state }));
     }
-    /*,
-    _entryChanged: function(allSummaries) {
-        this.setState(getState(allSummaries));
-    }
-    */
 });
--- src/request/components/request-view.jsx
+++ src/request/components/request-view.jsx
@@ -2,35 +2,10 @@ var React = require('react'),
     User = require('./request-user-view.jsx'),
     Filter = require('./request-filter-view.jsx'),
     Summary = require('./request-summary-view.jsx'),
-    Detail = require('./request-detail-view.jsx'),
-    EmitterMixin = require('../../lib/components/emitter-mixin.jsx');
-
-function getState(state) {
-    return {
-            selectedRequest: state ? state.selectedRequest : null
-        };
-}
+    Detail = require('./request-detail-view.jsx');
 
 module.exports = React.createClass({
-    mixins: [ EmitterMixin ],
-    componentDidMount: function() {
-        // TODO: Need to finish this
-        this.addListener('shell.request.detail.changed', this._requestDetailChanged);
-    },
-    getInitialState: function() {
-        return getState();
-    },
     render: function() {
-        var detail = '';
-        // TODO: Not sure where this should go yet... Seems too high to place this
-        if (this.state.selectedRequest) {
-            detail = (
-                <div className="col-md-10 col-md-offset-2 request-detail-holder-outer">
-                    <Detail />
-                </div>
-            );
-        }
-
         return (
             <div className="container-fluid">
                 <div className="row request-holder-outer">
@@ -43,12 +18,9 @@ module.exports = React.createClass({
                     <div className="col-md-2 request-filter-holder-outer">
                         <Filter />
                     </div>
-                    {detail}
+                    <Detail />
                 </div>
             </div>
         );
-    },
-    _requestDetailChanged: function(state) {
-        this.setState(getState({ selectedRequest: state }));
     }
 });
