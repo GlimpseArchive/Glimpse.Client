b71f8fcf7afd1258697de145348441611e613432
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-10-01 15:48:06 -0700

    Update access events with the sql statements that caused them

--- fake/fake-extension.js
+++ fake/fake-extension.js
@@ -63,7 +63,7 @@ function gernateShoppingCart() {
         action: 'CartSummary',
         route: generateRoute.normal('shoppingcart', 'cartsummary', null),
         activities: [
-            { type: 'data', access: 'SQL', operation: 'Select', target: 'Carts', affected: 1  }
+            { type: 'data', access: 'SQL', operation: 'Select', target: 'Carts', affected: 1, commmand: 'SELECT TOP (5) \n[Project1].[AlbumId] AS [AlbumId], \n[Project1].[GenreId] AS [GenreId], \n[Project1].[ArtistId] AS [ArtistId], \n[Project1].[Title] AS [Title], \n[Project1].[Price] AS [Price], \n[Project1].[AlbumArtUrl] AS [AlbumArtUrl]\nFROM ( SELECT \n    [Extent1].[AlbumId] AS [AlbumId], \n    [Extent1].[GenreId] AS [GenreId], \n    [Extent1].[ArtistId] AS [ArtistId], \n    [Extent1].[Title] AS [Title], \n    [Extent1].[Price] AS [Price], \n    [Extent1].[AlbumArtUrl] AS [AlbumArtUrl], \n    (SELECT \n        COUNT(1) AS [A1]\n        FROM [dbo].[OrderDetails] AS [Extent2]\n        WHERE [Extent1].[AlbumId] = [Extent2].[AlbumId]) AS [C1]\n    FROM [dbo].[Albums] AS [Extent1]\n)  AS [Project1]\nORDER BY [Project1].[C1] DESC'  }
         ]
     };
 }
@@ -74,13 +74,12 @@ function generateGenreMenu() {
         action: 'GenreMenu',
         route: generateRoute.normal('store', 'genremenu', null),
         activities: [
-            { type: 'data', access: 'SQL', operation: 'Select', target: 'Genres', affected: 10 }
+            { type: 'data', access: 'SQL', operation: 'Select', target: 'Genres', affected: 10, command: 'SELECT \n[Extent1].[GenreId] AS [GenreId], \n[Extent1].[Name] AS [Name], \n[Extent1].[Description] AS [Description]\nFROM [dbo].[Genres] AS [Extent1]' }
         ]
     };
 }
 
 function generateMvc() {
-    // '/Store/Browse?Genre={Rock}'
     for (var i = 0; i < chance.integerRange(5, 10); i++) {
         var genre = chance.word();
 
@@ -93,7 +92,7 @@ function generateMvc() {
                 { type: 'string', name: 'Genre', value: genre }
             ],
             activities: [
-                { type: 'data', access: 'SQL', operation: 'Select', target: 'Albums', affected: chance.integerRange(2, 50)  }
+                { type: 'data', access: 'SQL', operation: 'Select', target: 'Albums', affected: chance.integerRange(2, 50), command: 'SELECT \n[Project1].[GenreId] AS [GenreId], \n[Project1].[Name] AS [Name], \n[Project1].[Description] AS [Description], \n[Project1].[C1] AS [C1], \n[Project1].[AlbumId] AS [AlbumId], \n[Project1].[GenreId1] AS [GenreId1], \n[Project1].[ArtistId] AS [ArtistId], \n[Project1].[Title] AS [Title], \n[Project1].[Price] AS [Price], \n[Project1].[AlbumArtUrl] AS [AlbumArtUrl]\nFROM ( SELECT \n    [Limit1].[GenreId] AS [GenreId], \n    [Limit1].[Name] AS [Name], \n    [Limit1].[Description] AS [Description], \n    [Extent2].[AlbumId] AS [AlbumId], \n    [Extent2].[GenreId] AS [GenreId1], \n    [Extent2].[ArtistId] AS [ArtistId], \n    [Extent2].[Title] AS [Title], \n    [Extent2].[Price] AS [Price], \n    [Extent2].[AlbumArtUrl] AS [AlbumArtUrl], \n    CASE WHEN ([Extent2].[AlbumId] IS NULL) THEN CAST(NULL AS int) ELSE 1 END AS [C1]\n    FROM  (SELECT TOP (2) [Extent1].[GenreId] AS [GenreId], [Extent1].[Name] AS [Name], [Extent1].[Description] AS [Description]\n        FROM [dbo].[Genres] AS [Extent1]\n        WHERE [Extent1].[Name] = "Rock" ) AS [Limit1]\n    LEFT OUTER JOIN [dbo].[Albums] AS [Extent2] ON [Limit1].[GenreId] = [Extent2].[GenreId]\n)  AS [Project1] ORDER BY [Project1].[GenreId] ASC, [Project1].[C1] ASC' }
             ],
             child: [
                 gernateShoppingCart(),
@@ -109,9 +108,9 @@ function generateMvc() {
         mvcActions.push({ url: '/Store/Details/' + id, controller: 'Store', action: 'Details',
                 route: generateRoute.normal('store', 'details', id),
                 activities: [
-                    { type: 'data', access: 'SQL', operation: 'Select', target: 'Albums', affected: 1  },
-                    { type: 'data', access: 'SQL', operation: 'Select', target: 'Genres', affected: 1  },
-                    { type: 'data', access: 'SQL', operation: 'Select', target: 'Artists', affected: 1  }
+                    { type: 'data', access: 'SQL', operation: 'Select', target: 'Albums', affected: 1, command: 'SELECT TOP (2) \n[Extent1].[AlbumId] AS [AlbumId], \n[Extent1].[GenreId] AS [GenreId], \n[Extent1].[ArtistId] AS [ArtistId], \n[Extent1].[Title] AS [Title], \n[Extent1].[Price] AS [Price], \n[Extent1].[AlbumArtUrl] AS [AlbumArtUrl]\nFROM [dbo].[Albums] AS [Extent1]\nWHERE [Extent1].[AlbumId] = 1 /* @p0 */' },
+                    { type: 'data', access: 'SQL', operation: 'Select', target: 'Genres', affected: 1, command: 'SELECT \n[Extent1].[GenreId] AS [GenreId], \n[Extent1].[Name] AS [Name], \n[Extent1].[Description] AS [Description]\nFROM [dbo].[Genres] AS [Extent1]\nWHERE [Extent1].[GenreId] = 1 /* @EntityKeyValue1 */' },
+                    { type: 'data', access: 'SQL', operation: 'Select', target: 'Artists', affected: 1, command: 'SELECT \n[Extent1].[ArtistId] AS [ArtistId], \n[Extent1].[Name] AS [Name]\nFROM [dbo].[Artists] AS [Extent1]\nWHERE [Extent1].[ArtistId] = 1 /* @EntityKeyValue1 */' }
                 ],
                 child: [
                     gernateShoppingCart(),
@@ -125,7 +124,7 @@ function generateMvc() {
         { url: '/', controller: 'Home', action: 'Index',
                 route: generateRoute.normal('home', 'index', null),
                 activities: [
-                    { type: 'data', access: 'SQL', operation: 'Select', target: 'Albums', affected: chance.integerRange(2, 50)  }
+                    { type: 'data', access: 'SQL', operation: 'Select', target: 'Albums', affected: chance.integerRange(2, 50), command: 'SELECT TOP (5) \n[Project1].[AlbumId] AS [AlbumId], \n[Project1].[GenreId] AS [GenreId], \n[Project1].[ArtistId] AS [ArtistId], \n[Project1].[Title] AS [Title], \n[Project1].[Price] AS [Price], \n[Project1].[AlbumArtUrl] AS [AlbumArtUrl]\nFROM ( SELECT \n    [Extent1].[AlbumId] AS [AlbumId], \n    [Extent1].[GenreId] AS [GenreId], \n    [Extent1].[ArtistId] AS [ArtistId], \n    [Extent1].[Title] AS [Title], \n    [Extent1].[Price] AS [Price], \n    [Extent1].[AlbumArtUrl] AS [AlbumArtUrl], \n    (SELECT \n        COUNT(1) AS [A1]\n        FROM [dbo].[OrderDetails] AS [Extent2]\n        WHERE [Extent1].[AlbumId] = [Extent2].[AlbumId]) AS [C1]\n    FROM [dbo].[Albums] AS [Extent1]\n)  AS [Project1]\nORDER BY [Project1].[C1] DESC'  }
                 ],
                 child: [
                     gernateShoppingCart(),
@@ -134,8 +133,8 @@ function generateMvc() {
         { url: '/ShoppingCart/', controller: 'ShoppingCart', action: 'Index',
                 route: generateRoute.normal('shoppingcart', 'index', null),
                 activities: [
-                    { type: 'data', access: 'SQL', operation: 'Select', target: 'Carts', affected: 1 },
-                    { type: 'data', access: 'SQL', operation: 'Select', target: 'Carts', affected: 1  }
+                    { type: 'data', access: 'SQL', operation: 'Select', target: 'Carts', affected: 1, command: 'SELECT \n[Extent1].[RecordId] AS [RecordId], \n[Extent1].[CartId] AS [CartId], \n[Extent1].[AlbumId] AS [AlbumId], \n[Extent1].[Count] AS [Count], \n[Extent1].[DateCreated] AS [DateCreated]\nFROM [dbo].[Carts] AS [Extent1]\nWHERE [Extent1].[CartId] = "df0238d4-5bd4-49b5-97f0-9ba2c9957dc1" /* @p__linq__0 */' },
+                    { type: 'data', access: 'SQL', operation: 'Select', target: 'Carts', affected: 1, command: 'SELECT \n[GroupBy1].[A1] AS [C1]\nFROM ( SELECT \n    SUM([Filter1].[A1]) AS [A1]\n    FROM ( SELECT \n         CAST( [Extent1].[Count] AS decimal(19,0)) * [Extent2].[Price] AS [A1]\n        FROM  [dbo].[Carts] AS [Extent1]\n        INNER JOIN [dbo].[Albums] AS [Extent2] ON [Extent1].[AlbumId] = [Extent2].[AlbumId]\n        WHERE [Extent1].[CartId] = "df0238d4-5bd4-49b5-97f0-9ba2c9957dc1" /* @p__linq__0 */\n    )  AS [Filter1]\n)  AS [GroupBy1]' }
                 ],
                 child: [
                     gernateShoppingCart(),
@@ -144,7 +143,7 @@ function generateMvc() {
         { url: '/Store/', controller: 'Store', action: 'Index',
                 route: generateRoute.normal('store', 'index', null),
                 activities: [
-                    { type: 'data', access: 'SQL', operation: 'Select', target: 'Genres', affected: 10 }
+                    { type: 'data', access: 'SQL', operation: 'Select', target: 'Genres', affected: 10, command: 'SELECT \n[Extent1].[GenreId] AS [GenreId], \n[Extent1].[Name] AS [Name], \n[Extent1].[Description] AS [Description]\nFROM [dbo].[Genres] AS [Extent1]' }
                 ],
                 child: [
                     gernateShoppingCart(),
