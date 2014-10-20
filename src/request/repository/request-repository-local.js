f8312736b83adadbd2628c4281ec04bb7d681314
Bjorn Holdt <bjornholdt@Bjorns-MacBook-Pro-2.local>
2014-10-20 22:01:03 +1000

    Rework on Local Repository to enable flushing. Added mapping

--- src/request/repository/request-repository-local.js
+++ src/request/repository/request-repository-local.js
@@ -2,14 +2,24 @@ var glimpse = require('glimpse');
 var store = require('store.js');
 
 var _storeSummaryKey = 'glimpse.data.summary',
-    _storeDetailKey = 'glimpse.data.request';
+    _storeDetailKey = 'glimpse.data.request',
+    _mapRequestsSummary = 'glimpse.data.summary.maps';
 // store Found Summary
 (function() {
     //TODO: Need to complete
     //Push into local storage
     //address error handling, flushing out old data
     function storeFoundSummary(data) {
-      store.set(_storeSummaryKey, data);
+        var storeSummary = store.get(_storeSummaryKey) || [];
+        var mapping = store.get(_mapRequestsSummary) || {};
+        for (var i = 0; i < data.length; i++) {
+            var request = data[i];
+            storeSummary.push(request);
+            mapping[storeSummary.length - 1] = request.id;
+            mapping[request.id] = storeSummary.length - 1;
+        }
+        store.set(_storeSummaryKey, storeSummary);
+        store.set(_mapRequestsSummary, mapping);
     }
 
     glimpse.on('data.request.summary.found.remote', storeFoundSummary);
@@ -22,8 +32,7 @@ var _storeSummaryKey = 'glimpse.data.summary',
     //Push into local storage
     //address error handling, flushing out old data
     function storeFoundDetail(data) {
-      var key = _storeDetailKey + '.' + data.id;
-      store.set(key, data);
+      //needs to be redone like above to enable flushing
     }
 
     glimpse.on('data.request.detail.found.remote', storeFoundDetail);
@@ -35,15 +44,15 @@ module.exports = {
       //Pull from local storage
       //address error handling
         var data = store.get(_storeSummaryKey);
-        if(data && data.length > 0)
+        if(data){
           glimpse.emit('data.request.summary.found.local', data);
+        }
     },
     triggerGetDetailsFor: function(requestId) {
       //TODO: Need to complete
       //Pull from local storage
       //address error handling
-        var data = store.get(_storeDetailKey + '.' + requestId);
-        if(data && data.length > 0)
-          glimpse.emit('data.request.detail.found.local', data);
+      //needs to be redone like above, and then use mappings
     }
+
 };
