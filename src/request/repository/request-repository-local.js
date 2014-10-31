393f0011a0fcdc6ab77c9c1ea563eb5ac034c211
Bjorn Holdt <bjornholdt@vlan-2699-10-17-155-96.guest.wireless.sydney.edu.au>
2014-10-31 15:23:02 +1100

    Flush details extracrted to method. Spacing changes

--- src/request/repository/request-repository-local.js
+++ src/request/repository/request-repository-local.js
@@ -6,62 +6,66 @@ _storeDetailKey = 'glimpse.data.request.local',
 _storeDetailIndex = 'glimpse.data.request.local.index';
 
 (function() {
-  var storeSummary = store.get(_storeSummaryKey) || [];
-  var storeDetail = store.get(_storeDetailKey) || {};
-  function storeFoundSummary(data) {
-    for (var i = 0; i < data.length; i++) {
-      var request = data[i];
-      storeSummary.unshift(request);
-    }
-    flush(storeSummary);
-    store.set(_storeSummaryKey, storeSummary);
-  }
+   var storeSummary;
 
-  function flush(storeArray){
-    while(storeArray.length > 100){
-      var summary = storeArray.pop();
-    }
-  }
+   function storeFoundSummary(data) {
+      storeSummary = store.get(_storeSummaryKey) || [];
+      for (var i = 0; i < data.length; i++) {
+         var request = data[i];
+         storeSummary.unshift(request);
+      }
+      flush();
+      store.set(_storeSummaryKey, storeSummary);
+   }
 
-  glimpse.on('data.request.summary.found.remote', storeFoundSummary);
-  glimpse.on('data.request.summary.found.stream', storeFoundSummary);
+   function flush(){
+      while(storeSummary.length > 100){
+         storeSummary.pop();
+      }
+   }
+
+   glimpse.on('data.request.summary.found.remote', storeFoundSummary);
+   glimpse.on('data.request.summary.found.stream', storeFoundSummary);
 })();
 
 (function() {
+   var storeDetail, storeDetailIndex;
 
-  function storeFoundDetail(data) {
-    var storeDetail = store.get(_storeDetailKey) || {};
-    var storeDetailIndex = store.get(_storeDetailIndex) || [];
-    storeDetail[data.id] = data;
-    storeDetailIndex.unshift(data.id);
+   function storeFoundDetail(data) {
+      storeDetail = store.get(_storeDetailKey) || {};
+      storeDetailIndex = store.get(_storeDetailIndex) || [];
+      storeDetail[data.id] = data;
+      storeDetailIndex.unshift(data.id);
+      flush();
+      store.set(_storeDetailKey, storeDetail);
+      store.set(_storeDetailIndex, storeDetailIndex)
+   }
 
-    if(storeDetailIndex.length > 10){
-      var id = storeDetailsIndex.pop();
-      delete storeDetail[id];
-    }
+   function flush(){
+      while(storeDetailIndex.length > 10){
+         var id = storeDetailsIndex.pop();
+         delete storeDetail[id];
+      }
+   }
 
-    store.set(_storeDetailKey, storeDetail);
-    store.set(_storeDetailIndex, storeDetailIndex)
-  }
-
-  glimpse.on('data.request.detail.found.remote', storeFoundDetail);
+   glimpse.on('data.request.detail.found.remote', storeFoundDetail);
 })();
 
 module.exports = {
-  triggerGetLastestSummaries: function() {
-    var data = store.get(_storeSummaryKey);
-    if(data){
-      glimpse.emit('data.request.summary.found.local', data);
-    }
-  },
-  triggerGetDetailsFor: function(requestId) {
-    var data = store.get(_storeDetailKey);
-    if(data){
-      var requestDetail = data[requestId];
-      if(requestDetail){
-        glimpse.emit('data.request.detail.found.local', requestDetail);
+   triggerGetLastestSummaries: function() {
+      var data = store.get(_storeSummaryKey);
+      if(data){
+         glimpse.emit('data.request.summary.found.local', data);
       }
-    }
-  }
+   },
+   triggerGetDetailsFor: function(requestId) {
+      var data = store.get(_storeDetailKey);
+      if(data){
+         var requestDetail = data[requestId];
+         if(requestDetail){
+            glimpse.emit('data.request.detail.found.local', requestDetail);
+         }
+      }
+   }
 
 };
